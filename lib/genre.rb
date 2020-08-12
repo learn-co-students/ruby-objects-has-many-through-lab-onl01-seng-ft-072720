2a61cd3da0608fb957311ca33fb42d95f3b57577
Kyle Stough <kstough00@gmail.com>
2020-08-12 20:29:49 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,9 +1,31 @@
 class Genre
   
-  attr_accessor :name
+  attr_accessor :name, :song, :artist
+  
+  @@all = []
   
   def initialize(name)
     @name = name
+    @song = song
+    @artist = artist
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def songs
+    Song.all.select {|song| song.genre == self}
+  end
+  
+  def artists
+    Song.all.select {|song| song.artist == self}
+    songs.map {|song| song.artist}
+  end
+  
+  def new_song(name, artist)
+    Song.new(name, self, artist)
   end
   
 end
