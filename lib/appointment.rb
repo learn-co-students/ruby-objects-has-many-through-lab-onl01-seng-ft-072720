90ae8e8aa8b64d645d03bca1a4dbd9781e972ece
kelvin lanier <kelvinlanier@yahoo.com>
2020-08-13 18:56:39 +0000

    Done.

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,8 +4,8 @@ class Appointment
   @@all = []
   
   def initialize(date, patient, doctor)
+     @date = date
     @patient = patient
-    @date = date
     @doctor = doctor
     @@all << self
   end
