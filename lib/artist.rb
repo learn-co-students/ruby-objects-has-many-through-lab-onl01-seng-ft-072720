a0374a6d7f8a75acd5d05128c163f24fc8c9425e
Josh Raiborde <joshsrai@yahoo.com>
2020-08-17 00:02:51 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+class Artist 
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name) 
+    @name = name
+    @genres = []
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    genre.artists << self 
+    genre.songs << song  
+    song
+  end
+  
+  def songs 
+    Song.all.select{|song| song.artist == self }
+  end
+  
+  def genres
+    Genre.all.select{|genre| genre.artists.include?(self)}
+  end
+  
+end
